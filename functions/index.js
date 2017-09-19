ffc0412041ae3b2d8611cccde42af5a42bb28963
Thamyres Guedes <thataguedis@gmail.com>
2017-09-18 21:49:20 -0300

    Tô de boa, rodando o oug aqui.

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/timestamp', (request, response) => {
-    response.send('Olá');
+var expressLess = require('express-less');
+var pug = require('pug');
+
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.use('/assets/css', expressLess(__dirname + '/src/assets/less', { compress: true }));
+app.use('/assets/img', express.static(__dirname + '/src/assets/img'));
+app.use('/assets/fonts', express.static(__dirname + '/src/assets/fonts'));
+
+
+app.get('/', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+    response.render('index', { title: 'Pug - Hands ON', message:{header: 'Olá Mundo!', text: 'Esse é um template BEM SIMPLES, só para servir de base para o nosso laboratório de Pug. Se você está lendo essa mensagem é porque todas as dependencias estão rodando. Parabéns!'}})
 });
 
 exports.app = functions.https.onRequest(app);
