88e964791baefcdcd95bd84c82acfb64c11e9d1c
Thamyres Guedes <thataguedis@gmail.com>
2018-06-09 20:43:00 -0300

    🎨 Using store to work with data

---
+++ store/index.js
@@ -0,0 +1,21 @@
+export const state = () => ({
+  about_me: "<p>Atuo no mercado de trabalho, na Área de tecnologia já a mais de sete anos. Trabalho desenvolvendo User Interfaces para sites, sistemas e aplicativos, aplicando conceitos de UX. Além disso atuo como Front End developer para sites e sistemas web.</p><p>Tenho conhecimento das principais ferramentas de diagramação como Photoshop, Illustrator e Sketch, além de experiência na utilização de pré-compiladores como Sass, Less e Pug. Facilidade para aprender novas tecnologias e trabalhar em equipe.</p>",
+  skills: {
+    highlights: {
+      title: 'Destaques',
+      items: [ 'UI Design', 'Front-End Developer', 'UX Design' ]
+    },
+    knowledge: {
+      title: 'Conhecimento do Setor',
+      items: [ 'UX Design', 'Web Design Responsivo', 'Desenvolvimento de sites', 'Desenvolvimento de software', 'Web Development' ]
+    },
+    tools: {
+      title: 'Ferramentas e Tecnologias',
+      items: [ 'HTML5', 'HTML', 'CSS3', 'SASS', 'JQuery', 'Node.js', 'Adobe Illustrator', 'Adobe Photoshop', 'PHP', 'MySQL', 'WordPress', 'SQL', 'C#' ]
+    },
+    others: {
+      title: 'Outras Competências',
+      items: [ 'Codificação de front-end', 'Less', 'GruntJS', 'Smarty', 'Pug' ]
+    }
+  }
+})
