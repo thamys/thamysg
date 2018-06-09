ba71f454a9c5cc4e890bc616fd0963c750d6d298
Thamyres Guedes <thataguedis@gmail.com>
2018-06-09 20:42:30 -0300

    🎨 Always Be Componetizing

---
+++ components/index/ProfessionalXP.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  .professionalXP
+    h5.title Experiência Profissional
+    .timeline
+      each val in [1,2,3,4,5]
+        .job
+          img.cpnLogo(src="http://emiolo.com/wp-content/uploads/2017/08/logo_emiolo_site-2.png")
+          .info
+            h5.jobTitle UI/UX Designer e Front End Developer
+            h6.company eMiolo.com Soluções em Internet Ltda.
+            span.period Desde setembro 2016 até agora
+</template>
+
+<script>
+export default {
+  name: 'ProfessionalXP'
+}
+</script>
+
+<style lang="sass" scoped>
+  .timeline
+    position: relative
+    &::before
+      background-color: rgba(0, 0, 0, .54)
+      content: ""
+      height: 100%
+      left: 41px
+      position: absolute
+      width: 1px
+      z-index: 1
+    
+  .job
+    align-items: center
+    flex: 1
+      direction: row
+    padding-bottom: 16px
+    z-index: 2
+    .cpnLogo
+      background-color: #fff  
+      border: 2px solid #f4f4f4
+        radius: 50%
+      height: 80px
+      margin-right: 16px
+      object-fit: contain
+      width: 80px
+    .info
+      flex: 1
+      .jobTitle
+        font:
+          weight: 500
+        line-height: 1.5em
+      .company, .period
+        font:
+          size: .85em
+          weight: 100
+        line-height: 1.25em
+</style>
+
---
+++ components/index/TheSkills.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  .skills
+    h5.title Skills
+    div(v-for="(category, index) in skills" :class="index")
+      h6.categoryTitle {{category.title}}
+      ul.listSkills
+        li.lineSkill(v-for="skill in category.items")
+          span.skill {{skill}}
+</template>
+
+<script>
+export default {
+  name: 'TheSkills',
+  computed: {
+    skills () {
+      return this.$store.state.skills
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  .categoryTitle
+    margin: 1em 0 .5em 0
+  .listSkills
+    flex: 1
+      direction: row
+  .lineSkill
+    align-items: flex-end
+    color: rgba(0,0,0,.6)
+    flex-basis: 50%
+    font-size: .85em
+    margin-bottom: 8px
+    .skill
+      letter-spacing: .1em
+      text-transform: uppercase
+    .rate
+      flex: 1
+        direction: row
+      margin-left: 8px
+      .material-icons
+        font-size: 1.5em
+        margin-bottom: -1px
+</style>
+
